748d58d9ccf4803121b1210f73849f73a6991616
SwordYi <283901189@qq.com>
2018-12-19 16:24:28 +0800

    change to class

--- src/2048.py
+++ src/2048.py
@@ -4,219 +4,214 @@
     CreateTime:2018-12-17
     ModefiedTime:2018-12-19
 """
-import random,os
+import random, os
 
-_BEST_FILE = "bestScore.dat"
-matrix = [[0 for n in range(4)] for m in range(4)]
-score = 0
-best = 0
+class Game:
+	def __init__(self, _WinScore_ = 32, _BEST_FILE_ = "bestScore.dat"):
+		self.matrix = [[0 for n in range(4)] for m in range(4)]
+		self.score = 0
+		self._BEST_FILE_ = _BEST_FILE_
+		self.best = self.readFile()		
+		self._WinScore_ = _WinScore_
+		initNumFlag = 0
+		while True:
+			s = divmod(random.randrange(0, 16), 4)
+			if self.matrix[s[0]][s[1]] == 0:
+				self.matrix[s[0]][s[1]] = 2
+				initNumFlag += 1
+			if initNumFlag == 2:
+				break
 
-# 初始化游戏
-def init():
-    initNumFlag = 0    
-    readFile()
-    while True:
-        s = divmod(random.randrange(0, 16), 4)
-        if matrix[s[0]][s[1]] == 0:
-            matrix[s[0]][s[1]] = 2
-            initNumFlag += 1
-        if initNumFlag == 2:
-            break
+	# 0值判断
+	def notzero(self, s):
+		return s if s != 0 else ''
 
-# 0值判断
-def notzero(s):
-    return s if s != 0 else ''
+	# 显示结果
+	def display(self):
+		print(r"----------------------------2048----------------------------")
+		print(r"操作说明： 上（W） 下（S） 左（A） 右（D） 退出（Q）")
+		print(r"当前积分：%d，最高积分：%d" % (self.score, self.best))
+		print("\n\
+			┌────┬────┬────┬────┐\n\
+			│%4s│%4s│%4s│%4s│\n\
+			├────┼────┼────┼────┤\n\
+			│%4s│%4s│%4s│%4s│\n\
+			├────┼────┼────┼────┤\n\
+			│%4s│%4s│%4s│%4s│\n\
+			├────┼────┼────┼────┤\n\
+			│%4s│%4s│%4s│%4s│\n\
+			└────┴────┴────┴────┘"
+			%(self.notzero(self.matrix[0][0]), self.notzero(self.matrix[0][1]), self.notzero(self.matrix[0][2]), self.notzero(self.matrix[0][3]),
+			  self.notzero(self.matrix[1][0]), self.notzero(self.matrix[1][1]), self.notzero(self.matrix[1][2]), self.notzero(self.matrix[1][3]),
+			  self.notzero(self.matrix[2][0]), self.notzero(self.matrix[2][1]), self.notzero(self.matrix[2][2]), self.notzero(self.matrix[2][3]),
+			  self.notzero(self.matrix[3][0]), self.notzero(self.matrix[3][1]), self.notzero(self.matrix[3][2]), self.notzero(self.matrix[3][3]))
+			  )
 
-# 显示结果
-def display():
-    print(r"----------------------------2048----------------------------")
-    print(r"操作说明： 上（W） 下（S） 左（A） 右（D） 退出（Q）")
-    print(r"当前积分：%d，最高积分：%d" % (score, best))
-    print("\n\
-        ┌────┬────┬────┬────┐\n\
-        │%4s│%4s│%4s│%4s│\n\
-        ├────┼────┼────┼────┤\n\
-        │%4s│%4s│%4s│%4s│\n\
-        ├────┼────┼────┼────┤\n\
-        │%4s│%4s│%4s│%4s│\n\
-        ├────┼────┼────┼────┤\n\
-        │%4s│%4s│%4s│%4s│\n\
-        └────┴────┴────┴────┘"
-        %(notzero(matrix[0][0]), notzero(matrix[0][1]), notzero(matrix[0][2]), notzero(matrix[0][3]),
-          notzero(matrix[1][0]), notzero(matrix[1][1]), notzero(matrix[1][2]), notzero(matrix[1][3]),
-          notzero(matrix[2][0]), notzero(matrix[2][1]), notzero(matrix[2][2]), notzero(matrix[2][3]),
-          notzero(matrix[3][0]), notzero(matrix[3][1]), notzero(matrix[3][2]), notzero(matrix[3][3]))
-          )
+	# 往左移动
+	def moveLeft(self):
+		for i in range(4):
+			for j in range(3):
+				for k in range(j+1, 4):
+					if self.matrix[i][j] == 0 and self.matrix[i][k] > 0:
+						self.matrix[i][j] = self.matrix[i][k]
+						self.matrix[i][k] = 0
+					elif self.matrix[i][j] > 0 and self.matrix[i][j] == self.matrix[i][k]:
+						self.matrix[i][j] = self.matrix[i][j] + self.matrix[i][k]					
+						self.matrix[i][k] = 0
+						self.score += self.matrix[i][j]
+						break;
+					elif self.matrix[i][j] > 0 and self.matrix[i][k] > 0 \
+						and self.matrix[i][j] != self.matrix[i][k]:
+						self.matrix[i][j+1] = self.matrix[i][k]
+						if k != j + 1:
+							self.matrix[i][k] = 0
+						break;
 
-# 往左移动
-def moveLeft():
-	for i in range(4):
-		for j in range(3):
-			for k in range(j+1, 4):
-				if matrix[i][j] == 0 and matrix[i][k] > 0:
-					matrix[i][j] = matrix[i][k]
-					matrix[i][k] = 0
-				elif matrix[i][j] > 0 and matrix[i][j] == matrix[i][k]:
-					matrix[i][j] = matrix[i][j] + matrix[i][k]					
-					matrix[i][k] = 0
-					global score 
-					score += matrix[i][j]
-					break;
-				elif matrix[i][j] > 0 and matrix[i][k] > 0 \
-					and matrix[i][j] != matrix[i][k]:
-					matrix[i][j+1] = matrix[i][k]
-					if k != j + 1:
-						matrix[i][k] = 0
-					break;
+	# 往右移动
+	def moveRight(self):
+		for i in range(4):
+			for j in range(3, -1, -1):
+				for k in range(j-1, -1, -1):
+					if self.matrix[i][j] == 0 and self.matrix[i][k] > 0:
+						self.matrix[i][j] = self.matrix[i][k]
+						self.matrix[i][k] = 0
+					elif self.matrix[i][j] > 0 and self.matrix[i][j] == self.matrix[i][k]:
+						self.matrix[i][j] = self.matrix[i][j] + self.matrix[i][k]
+						self.matrix[i][k] = 0
+						self.score += self.matrix[i][j]
+						break;
+					elif self.matrix[i][j] > 0 and self.matrix[i][k] > 0 \
+						and self.matrix[i][j] != self.matrix[i][k]:
+						self.matrix[i][j-1] = self.matrix[i][k]
+						if k != j - 1:
+							self.matrix[i][k] = 0
+						break;				
 
-# 往右移动
-def moveRight():
-	for i in range(4):
-		for j in range(3, -1, -1):
-			for k in range(j-1, -1, -1):
-				if matrix[i][j] == 0 and matrix[i][k] > 0:
-					matrix[i][j] = matrix[i][k]
-					matrix[i][k] = 0
-				elif matrix[i][j] > 0 and matrix[i][j] == matrix[i][k]:
-					matrix[i][j] = matrix[i][j] + matrix[i][k]
-					matrix[i][k] = 0
-					global score
-					score += matrix[i][j]
-					break;
-				elif matrix[i][j] > 0 and matrix[i][k] > 0 \
-					and matrix[i][j] != matrix[i][k]:
-					matrix[i][j-1] = matrix[i][k]
-					if k != j - 1:
-						matrix[i][k] = 0
-					break;				
-
-# 往上移动
-def moveUp():
-	for j in range(4):
-		for i in range(3):
-			for k in range(i+1, 4):
-				if matrix[i][j] == 0 and matrix[k][j] > 0:
-					matrix[i][j] = matrix[k][j]
-					matrix[k][j] = 0
-				elif matrix[i][j] > 0 and matrix[i][j] == matrix[k][j]:
-					matrix[i][j] = matrix[i][j] + matrix[k][j]
-					matrix[k][j] = 0
-					global score
-					score += matrix[i][j]
-					break;
-				elif matrix[i][j] > 0 and matrix[k][j] > 0 \
-					and matrix[i][j] != matrix[k][j]:
-					matrix[i+1][j] = matrix[k][j]
-					if k != i + 1:
-						matrix[k][j] = 0
-					break;
-
-# 往下移动
-def moveDown():
-	for j in range(4):
-		for i in range(3, -1, -1):
-			for k in range(i-1, -1, -1):
-				if matrix[i][j] == 0 and matrix[k][j] > 0:
-					matrix[i][j] = matrix[k][j]
-					matrix[k][j] = 0
-				elif matrix[i][j] > 0 and matrix[i][j] == matrix[k][j]:
-					matrix[i][j] = matrix[i][j] + matrix[k][j]
-					matrix[k][j] = 0
-					global score
-					score += matrix[i][j]
-					break;
-				elif matrix[i][j] > 0 and matrix[k][j] > 0 \
-					and matrix[i][j] != matrix[k][j]:
-					matrix[i-1][j] = matrix[k][j]
-					if k != i - 1:
-						matrix[k][j] = 0
-					break;	
-
-# 判断是否还有空位
-def isHaveSpace():
-	for i in range(4):
+	# 往上移动
+	def moveUp(self):
 		for j in range(4):
-			if matrix[i][j] == 0:
-				return True
-	return False
+			for i in range(3):
+				for k in range(i+1, 4):
+					if self.matrix[i][j] == 0 and self.matrix[k][j] > 0:
+						self.matrix[i][j] = self.matrix[k][j]
+						self.matrix[k][j] = 0
+					elif self.matrix[i][j] > 0 and self.matrix[i][j] == self.matrix[k][j]:
+						self.matrix[i][j] = self.matrix[i][j] + self.matrix[k][j]
+						self.matrix[k][j] = 0
+						self.score += self.matrix[i][j]
+						break;
+					elif self.matrix[i][j] > 0 and self.matrix[k][j] > 0 \
+						and self.matrix[i][j] != self.matrix[k][j]:
+						self.matrix[i+1][j] = self.matrix[k][j]
+						if k != i + 1:
+							self.matrix[k][j] = 0
+						break;
 
-# 随机生成新的数字2
-def addRandomNum():    
-    while isHaveSpace() == True:
-        s = divmod(random.randrange(0, 16), 4)
-        if matrix[s[0]][s[1]] == 0:
-            matrix[s[0]][s[1]] = 2
-            return
-
-
-# 判断游戏状态，是否失败
-def isfailed():
-	for i in range(4):
+	# 往下移动
+	def moveDown(self):
 		for j in range(4):
-			if matrix[i][j] == 0 or \
-			i > 0 and matrix[i][j] == matrix[i-1][j] or \
-			i < 3 and matrix[i][j] == matrix[i+1][j] or \
-			j > 0 and matrix[i][j] == matrix[i][j-1] or \
-			j < 3 and matrix[i][j] == matrix[i][j+1] :
-				return False
-	print("--------------------------游戏失败--------------------------")
-	return True
+			for i in range(3, -1, -1):
+				for k in range(i-1, -1, -1):
+					if self.matrix[i][j] == 0 and self.matrix[k][j] > 0:
+						self.matrix[i][j] = self.matrix[k][j]
+						self.matrix[k][j] = 0
+					elif self.matrix[i][j] > 0 and self.matrix[i][j] == self.matrix[k][j]:
+						self.matrix[i][j] = self.matrix[i][j] + self.matrix[k][j]
+						self.matrix[k][j] = 0
+						self.score += self.matrix[i][j]
+						break;
+					elif self.matrix[i][j] > 0 and self.matrix[k][j] > 0 \
+						and self.matrix[i][j] != self.matrix[k][j]:
+						self.matrix[i-1][j] = self.matrix[k][j]
+						if k != i - 1:
+							self.matrix[k][j] = 0
+						break;	
 
-# 判断游戏状态，是否成功
-def isSuccess():	
-	for i in range(4):
-		for j in range(4):
-			if matrix[i][j] == 2048:
-				print("--------------------------游戏胜利--------------------------")
-				return True
-	return False
+	# 判断是否还有空位
+	def isHaveSpace(self):
+		for i in range(4):
+			for j in range(4):
+				if self.matrix[i][j] == 0:
+					return True
+		return False
 
-# 读取文件
-def readFile():
-	if os.path.exists(_BEST_FILE):
-		with open(_BEST_FILE, 'r') as f:
-			global best
-			best = int(f.read())
+	# 随机生成新的数字2
+	def addRandomNum(self):    
+	    while self.isHaveSpace() == True:
+	        s = divmod(random.randrange(0, 16), 4)
+	        if self.matrix[s[0]][s[1]] == 0:
+	            self.matrix[s[0]][s[1]] = 2
+	            return
 
-# 写入文件
-def writeFile():
-	with open(_BEST_FILE, 'w') as f:
-		f.write(str(best))
 
-# 更新最高分
-def updateBest():
-	global score, best
-	if score > best:
-		best = score
+	# 判断游戏状态，是否失败
+	def isfailed(self):
+		for i in range(4):
+			for j in range(4):
+				if self.matrix[i][j] == 0 or \
+				i > 0 and self.matrix[i][j] == self.matrix[i-1][j] or \
+				i < 3 and self.matrix[i][j] == self.matrix[i+1][j] or \
+				j > 0 and self.matrix[i][j] == self.matrix[i][j-1] or \
+				j < 3 and self.matrix[i][j] == self.matrix[i][j+1] :
+					return False
+		print("--------------------------游戏失败--------------------------")
+		return True
 
-# 主程序
-def main():
-    init()
-    display()
-    while True:
-    	d = input()
-    	if d == 'q' or d == 'Q':
-    		writeFile()
-    		print("退出游戏，本次积分为：%d" % (score))
-    		break
-    	elif d == 'w' or d == 'W':    		
-    		moveUp()    		
-    	elif d == 's' or d == 'S':    		
-    		moveDown()    		
-    	elif d == 'a' or d == 'A':
-    		moveLeft()   	    		
-    	elif d == 'd' or d == 'D':    		
-    		moveRight()    		
-    	else:
-    		continue
-    	updateBest()
-    	addRandomNum()
-    	display()
+	# 判断游戏状态，是否获胜
+	def isWin(self):	
+		for i in range(4):
+			for j in range(4):
+				if self.matrix[i][j] >= self._WinScore_:
+					print("--------------------------游戏胜利--------------------------")
+					return True
+		return False
 
-    	if isfailed() == True or isSuccess() ==True:
-    		writeFile()
-    		print("游戏结束，本次积分为：%d" % (score))
-    		break;
-    	
+	# 读取文件
+	def readFile(self):
+		bestScore = 0
+		if os.path.exists(self._BEST_FILE_):
+			with open(self._BEST_FILE_, 'r') as f:
+				bestScore = int(f.read())
+		return bestScore
+
+	# 写入文件
+	def writeFile(self):
+		with open(self._BEST_FILE_, 'w') as f:
+			f.write(str(self.best))
+
+	# 更新最高分
+	def updateBest(self):
+		if self.score > self.best:
+			self.best = self.score
+
+	# 主程序
+	def start(self):    
+		self.display()
+		while True:
+			d = input()
+			if d == 'q' or d == 'Q':
+				self.writeFile()
+				print("退出游戏，本次积分为：%d" % (self.score))
+				break
+			elif d == 'w' or d == 'W':    		
+				self.moveUp()    		
+			elif d == 's' or d == 'S':    		
+				self.moveDown()    		
+			elif d == 'a' or d == 'A':
+				self.moveLeft()   	    		
+			elif d == 'd' or d == 'D':    		
+				self.moveRight()    		
+			else:
+				continue
+			self.updateBest()
+			self.addRandomNum()
+			self.display()
+
+			if self.isfailed() == True or self.isWin() ==True:
+				self.writeFile()
+				print("游戏结束，本次积分为：%d" % (self.score))
+				break;
+	    	
 if __name__ == "__main__":
-    main()
+	game = Game(2048)
+	game.start()
